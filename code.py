9ffa6d34625fba61e18cca54a23b329a620bd983
Filip <filipszon123@gmail.com>
2024-08-06 16:10:28 +0200

    added adafruit_debouncer lib

--- code.py
+++ code.py
@@ -5,56 +5,58 @@ import usb_midi
 import adafruit_midi
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
+import adafruit_debouncer
 
 # MIDI initialization
 midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
 
-print("Orzel leci")  
+print("Eagle")  
 
 # Pin definitions for buttons
 button_pins = [board.GP6, board.GP7, board.GP8, board.GP9]
 
 # MIDI note mapping assigned to buttons
-note_mapping = [["C3"], ["D3"], ["E3"], ["F3"]]
+note_mapping = [["C3"], ["D3"], ["E3"], ["F3"], ["C4"], ["D4"], ["E4"], ["F4"]]
 
 # Button initialization
-buttons = []
+pins = []
+switches = []
 for bp in button_pins:
-    button = digitalio.DigitalInOut(bp)
-    button.direction = digitalio.Direction.INPUT
-    button.pull = digitalio.Pull.UP
-    buttons.append(button)
+    pin = digitalio.DigitalInOut(bp)
+    pin.direction = digitalio.Direction.INPUT
+    pin.pull = digitalio.Pull.UP
+    pins.append(pin)
+    # Debouncer initialization with DigitalInOut objects
+    switch = adafruit_debouncer.Button(pin, 10, 1000)
+    switches.append(switch)
 
-# States of buttons and their activations
-pressed_keys = [False] * len(button_pins)
-triggered_keys = [False] * len(button_pins)
+# Track the state of notes
+note_states = [False] * len(button_pins)
 
-# Debouncing time
-debounce_time = 0.05  # 50 ms
-last_time = [time.monotonic()] * len(button_pins)
+
+
 
 # Main loop
 while True:
-    current_time = time.monotonic()
-    
-    for i, btn in enumerate(buttons):
+    for i, switch in enumerate(switches):
+        switch.update()
+        
 
-        if current_time - last_time[i] > debounce_time:
-            pressed = not btn.value
 
-            if pressed != pressed_keys[i]:
-                pressed_keys[i] = pressed
-                last_time[i] = current_time
-                    
-                if pressed and not triggered_keys[i]:
-                    print(f"note {i} started")
-                    midi.send([NoteOn(note, 60) for note in note_mapping[i]])
-                    triggered_keys[i] = True
+        if switch.long_press:  # Button was pressed
+            print(note_mapping[i + 4])
+            midi.send([NoteOn(note, 60) for note in note_mapping[i + 4]])
+            note_states[i] = True
 
-                elif not pressed and triggered_keys[i]:
-                    print(f"note {i} stopped")
-                    midi.send([NoteOff(note, 0) for note in note_mapping[i]])
-                    triggered_keys[i] = False
+        elif switch.pressed and not switch.long_press:
+            print(note_mapping[i])
+            midi.send([NoteOn(note, 60) for note in note_mapping[i]])
+            note_states[i] = True  
+
+        elif switch.released:  # Button was released
+            print('not pressed')
+            midi.send([NoteOff(note, 0) for note in note_mapping[i]])
+            note_states[i] = False
 
     # Short delay before the next iteration
     time.sleep(0.01)
